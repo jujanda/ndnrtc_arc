fdd8d4513650215b0eb288f46682fcaf0b04edfc
Julian Janda <julian.janda@aau.at>
2019-10-22 14:30:27 +0200

    Added support for bigger/more evaluations to run script

--- cpp/resources/arcRunPostProcessing.sh
+++ cpp/resources/arcRunPostProcessing.sh
@@ -1,109 +1,340 @@
 #!/bin/bash
 
-sleep 18h
+sleep 9h
+# sleep 15m
 
 ./resources/arcPostProcess.sh /media/julian/extHD/ 0 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 0 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 0 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 0 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 0 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 0 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 0 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 0 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 0 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 0 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 1 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 1 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 1 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 1 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 1 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 1 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 1 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 1 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 1 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 1 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 2 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 2 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 2 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 2 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 2 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 2 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 2 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 2 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 2 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 2 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 3 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 3 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 3 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 3 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 3 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 3 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 3 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 3 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 3 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 3 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 4 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 4 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 4 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 4 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 4 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 4 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 4 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 4 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 4 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 4 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 5 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 5 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 5 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 5 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 5 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 5 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 5 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 5 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 5 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 5 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 6 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 6 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 6 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 6 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 6 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 6 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 6 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 6 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 6 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 6 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 7 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 7 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 7 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 7 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 7 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 7 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 7 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 7 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 7 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 7 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 8 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 8 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 8 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 8 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 8 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 8 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 8 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 8 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 8 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 8 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 9 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 9 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 9 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 9 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 9 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 9 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 9 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 9 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 9 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 9 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 10 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 10 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 10 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 10 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 10 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 10 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 10 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 10 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 10 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 10 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 11 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 11 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 11 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 11 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 11 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 11 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 11 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 11 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 11 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 11 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 12 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 12 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 12 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 12 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 12 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 12 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 12 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 12 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 12 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 12 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 13 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 13 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 13 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 13 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 13 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 13 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 13 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 13 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 13 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 13 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 14 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 14 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 14 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 14 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 14 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 14 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 14 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 14 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 14 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 14 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 15 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 15 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 15 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 15 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 15 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 15 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 15 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 15 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 15 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 15 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 16 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 16 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 16 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 16 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 16 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 16 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 16 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 16 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 16 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 16 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 17 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 17 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 17 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 17 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 17 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 17 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 17 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 17 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 17 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 17 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 18 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 18 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 18 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 18 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 18 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 18 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 18 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 18 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 18 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 18 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 19 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 19 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 19 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 19 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 19 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 19 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 19 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 19 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 19 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 19 9
+
 ./resources/arcPostProcess.sh /media/julian/extHD/ 20 0
 ./resources/arcPostProcess.sh /media/julian/extHD/ 20 1
 ./resources/arcPostProcess.sh /media/julian/extHD/ 20 2
 ./resources/arcPostProcess.sh /media/julian/extHD/ 20 3
 ./resources/arcPostProcess.sh /media/julian/extHD/ 20 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 20 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 20 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 20 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 20 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 20 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 21 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 22 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 23 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 24 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 25 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 26 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 27 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 28 9
+
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 0
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 1
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 2
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 3
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 4
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 5
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 6
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 7
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 8
+./resources/arcPostProcess.sh /media/julian/extHD/ 29 9
+
+# ./resources/arcPostProcess.sh /media/julian/extHD/ 99 0
+# ./resources/arcPostProcess.sh /media/julian/extHD/ 99 1
+# ./resources/arcPostProcess.sh /media/julian/extHD/ 99 2
+# ./resources/arcPostProcess.sh /media/julian/extHD/ 99 3
+# ./resources/arcPostProcess.sh /media/julian/extHD/ 99 4
